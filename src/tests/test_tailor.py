cd1d7a4ba708e1596d2c58d5ee62cdf941800879
Vuong Ho <hochivuong2002@gmail.com>
2024-08-14 19:40:34 -0400

    Add tailor resume by job description test

---
+++ src/tests/test_tailor.py
@@ -0,0 +1,15 @@
+import unittest
+import yaml
+from content.tailor import *
+
+class TailorResumeTest(unittest.TestCase):
+	def test_tailor_resume(self):
+		with open("./tests/data/test_raw.yaml", 'r') as file:
+			data = yaml.safe_load(file)
+
+		with open("./tests/data/test_job_description.txt", "r") as file:
+			jd = file.read()
+
+		result = tailor_resume_by_job_description(data, jd)
+
+		self.assertIsNotNone(result)
