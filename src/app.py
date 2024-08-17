5b9993c06783374feb3280dd46a17d407b8d8e45
Vuong Ho <hochivuong2002@gmail.com>
2024-08-17 02:08:04 -0400

    Quick ui changes and add resume template

--- src/app.py
+++ src/app.py
@@ -1,3 +1,4 @@
+import yaml
 from flask import Flask, render_template
 
 app = Flask(__name__,
@@ -6,7 +7,11 @@ app = Flask(__name__,
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    # Load the YAML content
+    with open('./templates/resume_template.yaml', 'r') as file:
+        resume_template = file.read()
+    
+    return render_template('index.html', resume_yaml_template=resume_template)
 
 if __name__ == '__main__':
     app.run(debug=True)
