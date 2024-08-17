38d9eb9844c8de10d08aed6cb23381bb0087ac80
Vuong Ho <hochivuong2002@gmail.com>
2024-08-17 19:20:24 -0400

    Add render resume tests

--- src/tests/test_api/test_resume.py
+++ src/tests/test_api/test_resume.py
@@ -62,17 +62,46 @@ class ResumeAPITestCase(unittest.TestCase):
         }
         self.assertEqual(response.get_json(), expected_response)
 
-    def test_post_generate_resume(self):
+    def test_post_render_resume(self):
         with open("./tests/data/test_resume.yaml", "r") as resume_file:
             resume_yaml = resume_file.read()
         json_payload = {
             "resume": resume_yaml,
         }
 
-        response = self.app.post("/api/resume/generate", json=json_payload)
+        response = self.app.post("/api/resume/render", json=json_payload)
 
         self.assertEqual(response.status_code, 200)
         expected_response = {
             "result": "valid"
+        }
+        self.assertEqual(response.get_json(), expected_response)
+
+    def test_post_render_resume_invalid(self):
+        json_payload = {
+            "resume": "name: Vuong Ho",
+        }
+
+        response = self.app.post("/api/resume/render", json=json_payload)
+
+        self.assertEqual(response.status_code, 200)
+        expected_response = {
+            "result": "valid"
+        }
+
+        expected_response = {"details": [
+            "Problem at [phone] with error [Field required]",
+            "Problem at [email] with error [Field required]",
+            "Problem at [website] with error [Field required]",
+            "Problem at [linkedin] with error [Field required]",
+            "Problem at [github] with error [Field required]",
+            "Problem at [education_list] with error [Field required]",
+            "Problem at [skills] with error [Field required]",
+            "Problem at [experience_list] with error [Field required]",
+            "Problem at [project_list] with error [Field required]",
+            "Problem at [awards_list] with error [Field required]"
+        ],
+            "error": "Schema validation failed",
+            "result": "invalid"
         }
         self.assertEqual(response.get_json(), expected_response)
