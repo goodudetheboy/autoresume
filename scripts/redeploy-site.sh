fd3e7101279c012b52a5c3f89e56e963506e86c6
Vuong Ho <hochivuong2002@gmail.com>
2024-08-20 01:13:46 -0400

    Fix redeploy site

---
+++ scripts/redeploy-site.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Change dir in to MLH Portfolio and fetch branch
+cd ~/autoresume
+git fetch && git reset origin/main --hard
+
+# Take down currently running containers
+sudo docker compose -f docker-compose.prod.yml down
+
+# Push new containers up
+sudo docker compose -f docker-compose.prod.yml up -d --build
