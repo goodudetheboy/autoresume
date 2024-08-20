19aca273581a4a1520768334cdd21c2155045432
Vuong Ho <hochivuong2002@gmail.com>
2024-08-20 12:07:32 -0400

    Add answer app question

---
+++ src/tests/test_content/test_answer.py
@@ -0,0 +1,28 @@
+import unittest
+
+import yaml
+
+from content.answer import *
+
+
+class AnswerTest(unittest.TestCase):
+    def setUp(self):
+        self.maxDiff = None
+
+    def test_answer_app_question(self):
+        """
+        Test the main function to answer app question based on resume and job description 
+        """
+        with open("./tests/data/test_resume.yaml", 'r') as file:
+            resume = yaml.safe_load(file)
+
+        with open("./tests/data/test_app_question.txt", 'r') as file:
+            test_question = file.read()
+
+        with open("./tests/data/test_job_description.txt", "r") as file:
+            job_description = file.read()
+
+        result = answer_app_question(test_question, resume, job_description)
+
+        print(result)
+        self.assertIsNone(result)
